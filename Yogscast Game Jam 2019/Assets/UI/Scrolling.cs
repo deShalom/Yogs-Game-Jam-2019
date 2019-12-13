1fcf1683d9f3bf08c0a6d0500ba8736765b233bd
LewisSim <57391605+LewisSim@users.noreply.github.com>
2019-12-13 21:28:05 +0000

    Scolling Script

---
+++ Yogscast Game Jam 2019/Assets/UI/Scrolling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scrolling : MonoBehaviour
+{
+    [SerializeField]
+    private float scollX = 0.5f, scollY = 0.5f;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float offsetX = Time.time * scollX;
+        float offsetY = Time.time * scollY;
+        GetComponent<Image>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
