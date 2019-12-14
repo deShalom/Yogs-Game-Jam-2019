9fe6de05c1f14f9f076e384265e62fa22e5af8f8
deShalom <jordanunderwoodisepic@gmail.com>
2019-12-14 00:24:39 +0000

    Re-added mysteriously deleted scripts.
    Thanks git!

---
+++ Yogscast Game Jam 2019/Assets/Jays Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    //Variables
+    public string scnName;
+
+    private void Start()
+    {
+
+    }
+
+    //Methods
+    void loadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+}
+//All code written by Jay Underwood (deShalom).
