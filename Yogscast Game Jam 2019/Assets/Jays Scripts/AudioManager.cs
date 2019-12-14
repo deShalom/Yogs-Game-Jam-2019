87dbdd14c7c34ceb454aceeba31f0400753f1ad5
deShalom <jordanunderwoodisepic@gmail.com>
2019-12-14 00:44:55 +0000

    Final commit for the night.
    Includes infrastructure for a soundtrack.

--- Yogscast Game Jam 2019/Assets/Jays Scripts/AudioManager.cs
+++ Yogscast Game Jam 2019/Assets/Jays Scripts/AudioManager.cs
@@ -8,6 +8,7 @@ public class AudioManager : MonoBehaviour
     //Variables
     public Slider eSlider, mSlider;
     public AudioSource music, sEffects;
+    public AudioClip[] SoundtrackClips;
 
     //Methods
 
@@ -22,12 +23,23 @@ public class AudioManager : MonoBehaviour
         PlayerPrefs.SetFloat("mVolume", mSlider.value);
         PlayerPrefs.SetFloat("eVolume", eSlider.value);
         Volume();
+        soundTrack();
     }
 
     public void Volume()
     {
         music.volume = PlayerPrefs.GetFloat("mVolume");
         sEffects.volume = PlayerPrefs.GetFloat("eVolume");
+    }
+
+    void soundTrack()
+    {
+        if (!music.isPlaying)
+        {
+            int randomClip = Random.Range(0, SoundtrackClips.Length);
+            music.clip = SoundtrackClips[randomClip];
+            music.Play();
+        }
     }
 }
 //All code written by Jay Underwood (deShalom).
