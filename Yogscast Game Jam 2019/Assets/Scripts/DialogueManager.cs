af54d6dff14d769d9b4c1e258212c08f48802cf3
Eric Da Silva <ericdragonball@gmail.com>
2019-12-15 18:30:47 +0000

    Answers and Questions implemented

--- Yogscast Game Jam 2019/Assets/Scripts/DialogueManager.cs
+++ Yogscast Game Jam 2019/Assets/Scripts/DialogueManager.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    public int charAlignment, charID;
-    public string contents, reasonTxt;
-    public string[] reason;
-    public TextAsset[] dialogueFile;
+    public int charAlignment, charID, reasonID;
+    public string contents, reasonTxt, answerContents;
+    public string[] reason, questions, answers;
+    public int[] questionID;
+    public TextAsset[] ReasonFile, AnswerFile, SlapFile, KickFile;
     public bool isLying;
+    //public Text
 
-    public StreamReader txtFile;
+    public StreamReader txtFile, answerFile;
     
     // Start is called before the first frame update
     void Start()
-    {           
-        //Testing on Start
-        GetReason();
+    {
+        
     }
 
       
@@ -26,12 +28,13 @@ public class DialogueManager : MonoBehaviour
         //Setup of alignment and generation of Reason
         charAlignment = Random.Range(0, 3);
         charID = Random.Range(0, 5);
-        txtFile = new StreamReader(Application.dataPath + "/Dialog Resources/" + dialogueFile[charAlignment].name + ".txt");
+        txtFile = new StreamReader(Application.dataPath + "/Dialog Resources/" + ReasonFile[charAlignment].name + ".txt");
         contents = txtFile.ReadToEnd();
         txtFile.Close();
         
         reason = contents.Split("\n"[0]);
-        reasonTxt = reason[Random.Range(0, reason.Length)];
+        reasonID = Random.Range(0, reason.Length);
+        reasonTxt = reason[reasonID];
         //Deciding if they're lying depending on chance from alignment.
         if (charAlignment == 0)
         {
@@ -52,6 +55,254 @@ public class DialogueManager : MonoBehaviour
                 isLying = true;
             }
         }
+        
+    }
+
+    public void GetQuestions()
+    {
+        switch (charAlignment)
+        {
+            case 0:
+                switch (reasonID)
+                {
+                    case 0:
+                        questions[0] = "What’s the brand of the food?";                        
+                        questions[1] = "What breed of dog was it?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 0;
+                            questionID[1] = 2;
+                        }else if (isLying)
+                        {
+                            questionID[0] = 1;
+                            questionID[1] = 3;
+                        }
+                        break;
+                    case 1:
+                        questions[0] = "What door number is your neighbour?";
+                        questions[1] = "What’s your neighbour’s name?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 4;
+                            questionID[1] = 6;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 5;
+                            questionID[1] = 7;
+                        }
+                        break;
+                    case 2:
+                        questions[0] = "Where do you work at?";
+                        questions[1] = "What were they talking about?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 8;
+                            questionID[1] = 10;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 9;
+                            questionID[1] = 11;
+                        }
+                        break;
+                    case 3:
+                        questions[0] = "What kind of dishes were they?";
+                        questions[1] = "That’s gross, dude. Do your dishes";
+                        if (!isLying)
+                        {
+                            questionID[0] = 12;
+                            questionID[1] = 14;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 13;
+                            questionID[1] = 15;
+                        }
+                        break;
+                    case 4:
+                        questions[0] = "Just the scraps? You couldn’t spare a little more?";
+                        questions[1] = "What did they say after?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 16;
+                            questionID[1] = 18;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 17;
+                            questionID[1] = 19;
+                        }
+                        break;
+                }
+                break;
+
+            case 1:
+                switch (reasonID)
+                {
+                    case 0:
+                        questions[0] = "What kind of trash was it?";
+                        questions[1] = "Where did you put it?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 0;
+                            questionID[1] = 2;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 1;
+                            questionID[1] = 3;
+                        }
+                        break;
+                    case 1:
+                        questions[0] = "What did they reply?";
+                        questions[1] = "Who was it?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 4;
+                            questionID[1] = 6;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 5;
+                            questionID[1] = 7;
+                        }
+                        break;
+                    case 2:
+                        questions[0] = "At what restaurant?";
+                        questions[1] = "What did she serve to you?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 8;
+                            questionID[1] = 10;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 9;
+                            questionID[1] = 11;
+                        }
+                        break;
+                    case 3:
+                        questions[0] = "How did they react?";
+                        questions[1] = "What did you say to them?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 12;
+                            questionID[1] = 14;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 13;
+                            questionID[1] = 15;
+                        }
+                        break;
+                    case 4:
+                        questions[0] = "What was that friend’s name?";
+                        questions[1] = "What did you do?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 16;
+                            questionID[1] = 18;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 17;
+                            questionID[1] = 19;
+                        }
+                        break;
+                }
+                break;
+
+            case 2:
+                switch (reasonID)
+                {
+                    case 0:
+                        questions[0] = "What street was it?";
+                        questions[1] = "What was her name?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 0;
+                            questionID[1] = 2;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 1;
+                            questionID[1] = 3;
+                        }
+                        break;
+                    case 1:
+                        questions[0] = "What's the name of the shelter?";
+                        questions[1] = "When did you do it?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 4;
+                            questionID[1] = 6;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 5;
+                            questionID[1] = 7;
+                        }
+                        break;
+                    case 2:
+                        questions[0] = "What is the name of the charity?";
+                        questions[1] = "How much did you donate?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 8;
+                            questionID[1] = 10;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 9;
+                            questionID[1] = 11;
+                        }
+                        break;
+                    case 3:
+                        questions[0] = "What organisation did you donate to?";
+                        questions[1] = "Can I see your Donor Card?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 12;
+                            questionID[1] = 14;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 13;
+                            questionID[1] = 15;
+                        }
+                        break;
+                    case 4:
+                        questions[0] = "What did you give him?";
+                        questions[1] = "Where was he located?";
+                        if (!isLying)
+                        {
+                            questionID[0] = 16;
+                            questionID[1] = 18;
+                        }
+                        else if (isLying)
+                        {
+                            questionID[0] = 17;
+                            questionID[1] = 19;
+                        }
+                        break;
+                }
+                break;
+
+            default:
+                print("Invalid input");
+                break;
+        }
+    }
+
+    public void GetAnswers()
+    {
+        answerFile = new StreamReader(Application.dataPath + "/Dialog Resources/" + AnswerFile[charAlignment].name + ".txt");
+        answerContents = answerFile.ReadToEnd();
+        print("AnswersRead");
+        answerFile.Close();
+
+        answers = answerContents.Split("\n"[0]);
 
     }
 }
